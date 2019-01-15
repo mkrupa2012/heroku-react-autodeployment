391e2b3fe758343b49944b3aed3346288307dc47
Marcin Krupa <mkrupa2012@gmail.com>
2019-01-15 10:41:27 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     context: path.join(__dirname, "app"),
     entry: [
-        './app/js/index.js', './app/sass/app.scss'
+        './js/index.js', './sass/app.scss'
     ],
     resolve: {
         extensions: ['.webpack.js', '.web.js', '.ts', '.js']
